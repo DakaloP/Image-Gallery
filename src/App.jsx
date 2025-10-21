6313a72a94024561e1a370620f42afe2e5b5f6e2
DakaloP <dam101@DAs-MacBook-Air-5.local>
2025-10-21 12:39:09 +0200

    changes

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Gallery from './Components/Gallery'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+       <Gallery></Gallery>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
