0e8c07ca465ab27a0c719b5ac723e3963a614a89
DakaloP <dam101@DAs-MacBook-Air-5.local>
2025-10-21 16:03:57 +0200

    changes

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,31 +1,40 @@
 import { sculptureList } from "../API/Data";
 import { useState } from "react";
 
-export default function Gallery(){
-    const [index, setIndex] = useState (0);
-    function handleClick(){
-     setIndex((index + 1));
-    }
-     
-    function previousClick(){
-        setIndex((index - 1));
-        
-     }
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
 
-     
-    let sculpture = sculptureList[index];
-    return(
-     <>
-     <button onClick={previousClick}>previous</button>
-     <button onClick={handleClick}>Next</button>
-     <h2><i>{sculpture.name}</i>
-     by <strong>{sculpture.name}</strong>
-     </h2>
-     <h3>
-        ({index + 1} of {sculptureList.length}) 
-     </h3>
-    <img src ={sculpture.url} alt={sculpture.alt} />
-    <p>{sculpture.description}</p>
-     </>
-    )
+  function handleClick() {
+    if (index < sculptureList.length - 1) {
+      setIndex(index + 1);
+    } else {
+      alert("You’re at the last sculpture!");
+    }
+  }
+
+  function previousClick() {
+    if (index > 0) {
+      setIndex(index - 1);
+    } else {
+      alert("You’re at the first sculpture!");
+    }
+  }
+
+  let sculpture = sculptureList[index];
+
+  return (
+    <>
+      <button onClick={previousClick}>Previous</button>
+      <button onClick={handleClick}>Next</button>
+
+      <h2>
+        <i>{sculpture.name}</i> by <strong>{sculpture.artist}</strong>
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <img src={sculpture.url} alt={sculpture.alt} />
+      <p>{sculpture.description}</p>
+    </>
+  );
 }
